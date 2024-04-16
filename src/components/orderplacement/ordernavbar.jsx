a36700bfc4577a22c044bfb7789db93b591e9487
Sarthak Shrestha <sarthakstha24@gmail.com>
2024-04-16 16:21:58 +0545

    Margin

--- src/components/orderplacement/ordernavbar.jsx
+++ src/components/orderplacement/ordernavbar.jsx
@@ -7,10 +7,10 @@ function OrderNavbar() {
       <Navbar />
       <div className="p-7 text-slate-200 mx-auto relative">
         <div>
-          <h1 className="text-center mx-auto text-4xl font-archivo mt-9">
+          <h1 className="text-center ml-[100px] text-4xl font-archivo mt-9">
             Place your order through WordScape
           </h1>
-          <ul className="steps steps-vertical ml-[450px] mt-[60px] lg:steps-horizontal sm:steps-vertical">
+          <ul className="steps steps-vertical ml-[380px] mt-[60px] lg:steps-horizontal sm:steps-vertical">
             <li className="step step-primary" data-content="●">Place an Order</li>
             <li className="step">Choose Lamination</li>
             <li className="step">Purchase</li>
