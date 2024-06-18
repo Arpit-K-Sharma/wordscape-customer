35c761607d67ee91db2a036d30819663489d03cd
Sarthak Shrestha <sarthakstha24@gmail.com>
2024-06-18 22:41:44 +0545

    Added Backend URL

--- src/components/axiosInstance.jsx
+++ src/components/axiosInstance.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8081",
+  baseURL: "https://frabjous-clafoutis-7cf3e7.netlify.app/api",
 });
 
 axiosInstance.interceptors.request.use(
